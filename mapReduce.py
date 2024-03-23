2159a35983d1d3a536999a9d1bd050efe8fea73d
Memo5588 <151465762+Memo5588@users.noreply.github.com>
2024-03-23 16:14:32 +0300

    Version 4 - User Input Implementation

--- mapReduce.py
+++ mapReduce.py
@@ -11,9 +11,11 @@ def preprocess_data(file_path):
         with open(file_path, 'r') as file:
             reader = csv.reader(file)
             for row in reader:
+                # Check for empty rows
                 if len(row) < 1:
                     raise ValueError("Invalid data format: Empty row detected.")
                 passenger_id = row[0]
+                # Check for empty passenger IDs
                 if not passenger_id or not passenger_id.strip():
                     raise ValueError("Invalid data format: Empty passenger ID detected.")
                 processed_data.append(passenger_id)
@@ -38,7 +40,7 @@ def shuffle(mapped_result, num_reducers):
         shuffled_result[hashed_key].append((key, value))
     return shuffled_result.items()
 
-# Reduce Phase  -  the shuffled data is aggregated to produce the final result.
+# Reduce Phase -  the shuffled data is aggregated to produce the final result.
 def reducer(shuffled_result):
     reduced_result = defaultdict(int)
     for _, key_value_list in shuffled_result:
@@ -46,21 +48,24 @@ def reducer(shuffled_result):
             reduced_result[passenger_id] += count
     return reduced_result
 
-# Output Formatting  - the format_output function formats and displays the final output.
+# Output Formatting - the format_output function formats and displays the final output.
 def format_output(result, n=1):
     top_passengers = heapq.nlargest(n, result.items(), key=lambda x: x[1])
     for passenger_id, count in top_passengers:
         print(f"Passenger ID: {passenger_id} - Total Flights: {count}")
 
 if __name__ == "__main__":
+    # Prompt User input for file path
+    file_path = input("Enter the file path: ")
+    
     # Data preprocessing
-    flight_data = preprocess_data("AComp_Passenger_data_no_error.csv")
+    flight_data = preprocess_data(file_path)
     
     if flight_data:
         # Map Phase
         mapped_result = mapper(flight_data)
         
-        # Determine the number of reducers based on available CPU cores
+        # determine the number of reducers based on available CPU cores
         num_cores = multiprocessing.cpu_count()
         num_reducers = max(1, num_cores - 1)  # Use all cores except one for reducers
         
